0b0c49d9a273ea389af34b9c1f4904bd19de2e5a
QYT <2502053485@qq.com>
2019-11-07 01:16:55 +0800

    Site updated: 2019-11-07 01:16:50

---
+++ dist/music.js
@@ -0,0 +1,32 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: true,
+    audio: [
+      {
+        name: "斯德哥摩尔情人",
+        artist: '陈奕迅',
+        url: 'http://music.163.com/song/media/outer/url?id=27483204.mp3',
+      },
+      {
+        name: '少女的祈祷',
+        artist: '杨千嬅',
+        url: 'http://music.163.com/song/media/outer/url?id=329593497.mp3',
+      },
+      {
+        name: '钟无艳',
+        artist: '谢安琪',
+        url: 'http://music.163.com/song/media/outer/url?id=400074284.mp3',
+      },
+      {
+        name: '倒刺',
+        artist: '薛凯琪',
+        url: 'http://music.163.com/song/media/outer/url?id=306763.mp3',
+      },
+      {
+        name: '厌弃',
+        artist: '许廷铿',
+        url: 'http://music.163.com/song/media/outer/url?id=174120.mp3',
+      }
+    ]
+});
