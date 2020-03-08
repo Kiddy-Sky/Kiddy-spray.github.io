15ca795939c11df929e2d09fcc9835f05fcf8227
QYT <2502053485@qq.com>
2020-03-08 12:53:24 +0800

    Site updated: 2020-03-08 12:53:23

--- dist/music.js
+++ dist/music.js
@@ -14,31 +14,31 @@ const ap = new APlayer({
       {
         name: '少女的祈祷',
         artist: '杨千嬅',
-        url: 'https://Kiddy-spray.github.io/dist/songs/少女的祈祷 -- 杨千嬅 .mp3',
+        url: 'https://Kiddy-spray.github.io/dist/songs/少女的祈祷%20--%20杨千嬅%20.mp3',
       # url: 'http://music.163.com/song/media/outer/url?id=329593497.mp3',
       },
       {
         name: '钟无艳',
         artist: '谢安琪',
-        url: 'https://Kiddy-spray.github.io/dist/songs/钟无艳 -- 谢安琪 .mp3',
+        url: 'https://Kiddy-spray.github.io/dist/songs/钟无艳%20--%20谢安琪%20.mp3',
       # url: 'http://music.163.com/song/media/outer/url?id=308353.mp3',
       },
       {
         name: '倒刺',
         artist: '薛凯琪',
-        url: 'https://Kiddy-spray.github.io/dist/songs/倒刺 -- 薛凯琪 .mp3',
+        url: 'https://Kiddy-spray.github.io/dist/songs/倒刺%20--%20薛凯琪%20.mp3',
       # url: 'http://music.163.com/song/media/outer/url?id=306763.mp3',
       },
       {
         name: '厌弃',
         artist: '许廷铿',
-        url: 'https://Kiddy-spray.github.io/dist/songs/厌弃 -- 许廷铿 .mp3',
+        url: 'https://Kiddy-spray.github.io/dist/songs/厌弃%20--%20许廷铿%20.mp3',
       # url: 'http://music.163.com/song/media/outer/url?id=174120.mp3',
       },
       {
         name: '阁楼',
         artist: '陈慧琳',
-        url: 'https://Kiddy-spray.github.io/dist/songs/阁楼 -- 陈慧琳 .mp3',
+        url: 'https://Kiddy-spray.github.io/dist/songs/阁楼%20--%20陈慧琳%20.mp3',
       # url: 'http://music.163.com/song/media/outer/url?id=213791.mp3',
       }
     ]
